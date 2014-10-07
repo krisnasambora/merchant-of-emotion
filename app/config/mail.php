e9bc58210e2e0b193721e5ac84d1767211116dc0
krisnasambora <krisnasambora@yahoo.com>
2014-10-07 09:47:33 +0700

    gitignore test

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'krisnasambora@yahoo.com', 'name' => null),
+	'from' => array('address' => 'krisnasambora66666666@yahoo.com', 'name' => null),
 
 	/*
 	|--------------------------------------------------------------------------
