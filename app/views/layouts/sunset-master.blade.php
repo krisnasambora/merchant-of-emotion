e354a51525db4951880a0fb581ec8e980d373842
krisnasambora <krisnasambora@yahoo.com>
2014-11-04 23:30:33 +0700

    jInvertScroll initialisation.

--- app/views/layouts/sunset-master.blade.php
+++ app/views/layouts/sunset-master.blade.php
@@ -5,6 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		{{ HTML::style('css/bootstrap.min.css'); }}
 		{{ HTML::script('//code.jquery.com/jquery-1.11.0.min.js'); }}
+		{{ HTML::script('js/jquery.jInvertScroll.min.js'); }}
 		@yield('head-data')
 		@yield('tab-title')
 	</head>
