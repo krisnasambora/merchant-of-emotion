158b05e24a0e6e70b2edfb3ec0c735f02ae3a25b
krisnasambora <krisnasambora@yahoo.com>
2014-12-11 19:50:57 +0700

    beta migration fix

--- app/database/migrations/2014_12_06_041322_seed_tbsd_shows_table.php
+++ app/database/migrations/2014_12_06_041322_seed_tbsd_shows_table.php
@@ -13,18 +13,18 @@ class SeedTbsdShowsTable extends Migration {
 	public function up()
 	{
 		DB::table('TBSD_shows')->insert(array(
-		    array('show_id' => 1, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 1, 'class' => 2, 'confirmed' => 0, 'capacity' => 200),
-		    array('show_id' => 2, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 2, 'class' => 2, 'confirmed' => 0, 'capacity' => 200),
-		    array('show_id' => 3, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 3, 'class' => 2, 'confirmed' => 0, 'capacity' => 200),
-		    array('show_id' => 4, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 4, 'class' => 2, 'confirmed' => 0, 'capacity' => 200),
-		    array('show_id' => 5, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 5, 'class' => 2, 'confirmed' => 0, 'capacity' => 200),
-		    array('show_id' => 6, 'class' => 1, 'confirmed' => 0, 'capacity' => 300),
-		    array('show_id' => 6, 'class' => 2, 'confirmed' => 0, 'capacity' => 200)
+		    array('show_id' => 1, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 1, 'class' => 2, 'confirmed' => 0, 'capacity' => 234),
+		    array('show_id' => 2, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 2, 'class' => 2, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 3, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 3, 'class' => 2, 'confirmed' => 0, 'capacity' => 234),
+		    array('show_id' => 4, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 4, 'class' => 2, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 5, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 5, 'class' => 2, 'confirmed' => 0, 'capacity' => 234),
+		    array('show_id' => 6, 'class' => 1, 'confirmed' => 0, 'capacity' => 226),
+		    array('show_id' => 6, 'class' => 2, 'confirmed' => 0, 'capacity' => 226)
 		));
 	}
 
