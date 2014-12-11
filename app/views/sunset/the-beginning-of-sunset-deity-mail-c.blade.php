fb45af2ed3310048160aaa9911270a2bd09a6694
krisnasambora <krisnasambora@yahoo.com>
2014-12-11 11:17:46 +0700

    previous commit necessary files

---
+++ app/views/sunset/the-beginning-of-sunset-deity-mail-c.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+		{{ HTML::style('css/bootstrap.min.css'); }}
+		{{ HTML::script('js/bootstrap.min.js'); }}
+	</head>
+	<body>
+        <p>Teruntuk, {{$name}}</p>
+        <br/>
+        <p>Terima kasih telah melakukan pembayaran pemesanan tiket Pementasan Teater Sunset Deity. Berikut terlampir bukti pembayaran untuk nantinya ditukarkan menjadi tiket masuk teater. Sampai bertemu di Teater Tertutup Dago Tea House!</p>
+		<br/>
+		<p>Salam hangat,</p>
+		<p>Merchant of Emotion</p>
+	</body>
+</html>
---
+++ app/views/sunset/the-beginning-of-sunset-deity-mail-d.blade.php
@@ -0,0 +1,33 @@
+<html>
+    <body style="font-family: 'Courier New';">
+        <div style="margin-left:31%;width:500px;height:200px;">
+            <img src="http://news.bbcimg.co.uk/media/images/71832000/jpg/_71832498_71825880.jpg" align="center" style="max-width:100%;max-height:100%;"></img>
+        </div>
+        <p align="right">Kode Pemesanan</p>
+        <div class="box"style="float: right; width:150px; height:70px;border-style: solid; border-width: 1px;">
+            <p align="center" style="font-size:25px;font-weight:bold;"> 0021 </p>
+        </div>
+        <p>Nama        :  cobol </p>
+        <p>No Telepon  :  cobol </p>
+        <p>Email       :  cobol </p>
+        <p>No ID       :  cobol </p><br/>
+        <div style="margin-left: 35%;text-align:center;width:400px;height:360px;">
+            <p align="center">Posisi Duduk </p>
+            <div class="box"style="margin-left:32%;width:140px; height:110px;border-style: solid; border-width: 1px;">
+                <p align="center" style="font-size:25px;font-weight:bold;"> si cobol </p>
+            </div>
+            <br/>
+            <br/>
+            <p align="center">Jumlah Tiket </p>
+            <div class="box"style="margin-left:32%;width:140px; height:110px;border-style: solid; border-width: 1px;">
+                <p align="center" style="font-size:25px;font-weight:bold;"> si cobol </p>
+            </div>
+        </div>
+        <p>Ketentuan :</p><br/>
+        <p>1. Bukti Pembayaran ini berlaku untuk penukaran tiket asli, bukan sebagai tiket masuk.</p>
+        <p>2. Print Bukti Pembayaran ini untuk ditukarkan dengan tiket masuk.</p>
+        <p>3. Pihak Merchant of Emotion tidak bertanggung jawab atas kehilangan Bukti Pembayaran ini.</p>
+        <p>4. Penukaran Bukti Pembayaran ini berlaku hari H pertunjukan (atau tunggu info lebih lanjut)</p>
+        <p>5. Pemesan akan membebaskan pihak penyelenggara dari segala macam bentuk tuntutan hukum yang timbul karena kelalaian dan/atau kesalahan pemesan sendiri.</p>
+    </body>
+</html>
